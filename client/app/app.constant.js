90efd1313a57b7ed46b8571e09e7db071ab650d1
Jameson Parker <parkej60@gmail.com>
2016-01-07 10:01:34 -0600

    Initial Commit.

---
+++ client/app/app.constant.js
@@ -0,0 +1,9 @@
+(function(angular, undefined) {
+'use strict';
+
+angular.module('jamesonwparkerApp.constants', [])
+
+.constant('appConfig', {userRoles:['guest','user','admin']})
+
+;
+})(angular);
